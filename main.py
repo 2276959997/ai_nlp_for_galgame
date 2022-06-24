c737a439260ab9347aa05b768aaee905cf93f3e7
一六二十六 <2276959997@qq.com>
2022-06-25 07:43:05 +0800

    update unicode-utf8

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
 import websockets
 import json
+import chardet
 
 from utils.config_factory import GetConfig
 from app_interface.senticment_factory import GetAi
-
 
 # 获取项目配置
 config = GetConfig.get_config('ini_config')
@@ -14,27 +14,59 @@ ai = GetAi.get_ai(config)
 GAME_CLIENT = set()
 
 
+def handle_msg(msg):
+    """预处理msg"""
+    # 如果是b'’就单程unicode来处理
+    is_bytes = False
+    if isinstance(type(msg), type(bytes)):
+        msg = msg.decode('unicode-escape')
+        is_bytes =True
+    try:
+        msg = json.loads(msg)
+    except json.decoder.JSONDecodeError as JErr:
+        print(f"不能序列化为json的字符串{msg}")
+    finally:
+        pass
+
+    return is_bytes, isinstance(type(msg), type(dict)), msg
+
+
 async def ai_output(websocket):
     global GAME_CLIENT
+
     async for msg in websocket:
-        msg = str(msg).encode('utf-8').decode("unicode_escape")
-        msg = json.loads(msg)
-        if 'MsgEvet' in msg:
-            event = msg['MsgEvet']
-            # TODO 返回分装成接口
-            if event == 'str_emotion_classify':
-                send_msg = ai.get_str_classify(msg['MsgText'])
+        is_bytes, is_dict, msg = handle_msg(msg)
 
+        # dict类型，json.loads成功
+        if is_dict:
+            # 非字符串
+            if 'MsgEvent' in msg.keys():
+                event = msg['MsgEvent']
+                # TODO 返回分装成接口
+                if event == 'strEmotionClassify':
+                    send_msg = ai.get_str_classify(msg['MsgText'])
+
+                    send_msg_dic = dict()
+                    send_msg_dic['emotionType'] = send_msg[0]
+                    send_msg_dic['confidence'] = send_msg[1]
+                    send_msg_dic['subLabel'] = send_msg[2]
+                    send_msg_dic['replies'] = send_msg[3]
+
+                    # print("return c#: ", json.dumps(send_msg_dic).encode('utf-8').decode("unicode_escape"))
+                    if is_bytes:
+                        await websocket.send(json.dumps(send_msg_dic).encode('utf-8').decode("unicode_escape"))
+                    else:
+                        await websocket.send(json.dumps(send_msg_dic))
+
+            else:
                 send_msg_dic = dict()
-                send_msg_dic['emotion_type'] = send_msg[0]
-                send_msg_dic['confidence'] = send_msg[1]
-                send_msg_dic['sub_label'] = send_msg[2]
-                send_msg_dic['replies'] = send_msg[3]
-
-                print(json.dumps(send_msg_dic).encode('utf-8').decode("unicode_escape"))
-                await websocket.send(json.dumps(send_msg_dic))
+                send_msg_dic['err'] = "unknown message"
+                if is_bytes:
+                    await websocket.send(json.dumps(msg).encode('utf-8').decode("unicode_escape"))
+                else:
+                    await websocket.send(json.dumps(msg))
         else:
-            await websocket.send('unknow message')
+            await websocket.send(msg)
 
 
 async def main():
@@ -44,7 +76,6 @@ async def main():
 
 
 if __name__ == '__main__':
-
     # 根据项目根目录的app.ini实例化一个ai对象
     # ai = GetAi.get_ai(config)
 
